10dda0ed8264ebcb398d019868799fd1ae8f2662
Jake Nagle <jake.nagle54@gmail.com>
2025-06-25 17:42:29 +0100

    Exercise - Show favourite icon on Movie Details page..

--- src/components/headerMovie/index.tsx
+++ src/components/headerMovie/index.tsx
@@ -6,6 +6,8 @@ import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import HomeIcon from "@mui/icons-material/Home";
 import { MovieDetailsProps } from "../../types/interfaces";
+import Avatar from "@mui/material/Avatar";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 
 const styles = {
     root: {  
@@ -14,17 +16,27 @@ const styles = {
     alignItems: "center",
     flexWrap: "wrap",
     padding: 1.5,
-  },
-};
+    },
+    avatar: {
+    backgroundColor: "rgb(255, 0, 0)",
+    },
+  };
 
 const MovieHeader: React.FC<MovieDetailsProps> = (movie) => {
+  const favouriteMovies = JSON.parse(localStorage.getItem("favourites") || '[]');
 
   return (
     <Paper component="div" sx={styles.root}>
       <IconButton aria-label="go back">
         <ArrowBackIcon color="primary" fontSize="large" />
       </IconButton>
-
+      {
+        favouriteMovies ? (
+          <Avatar sx={styles.avatar}>
+            <FavoriteIcon />
+          </Avatar>
+        ) : null
+      }
       <Typography variant="h4" component="h3">
         {movie.title}{"   "}
         <a href={movie.homepage}>
