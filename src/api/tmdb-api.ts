24939ff0322c0ce22de9a04cfc7dcba4b1c10ffa
Jake Nagle <jake.nagle54@gmail.com>
2025-07-28 16:31:31 +0100

    Fix upcoming movies page.

--- src/api/tmdb-api.ts
+++ src/api/tmdb-api.ts
@@ -63,11 +63,17 @@ export const getMovieReviews = (id: string | number) => {
     });
 };
 
-// Added to get Upcoming Movies for Excercise Lab-Movie_App-Part3 
+// Added to get Upcoming Movies for Excercise Lab-Movie_App-Part3 - Updated for Part 4
 export const getUpcomingMovies = () => {
   return fetch(
     `https://api.themoviedb.org/3/movie/upcoming?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&page=1`
-  )
-    .then(res => res.json())
-    .then(json => json.results);
+  ).then((response) => {
+    if (!response.ok) {
+      throw new Error(`Failed to get upcoming movie data. Response status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .catch((error) => {
+    throw error
+  });
 };
